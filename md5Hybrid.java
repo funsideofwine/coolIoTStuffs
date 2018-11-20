f76447462540bee5a31adb6774280f924afe4746
Rolly Falco Villacacan <32959267+funsideofwine@users.noreply.github.com>
2018-11-20 16:30:58 +0300

    Update md5Hybrid.java

--- md5Hybrid.java
+++ md5Hybrid.java
@@ -5,138 +5,85 @@ import java.security.*;
 
 
 
-public class md5Hybrid_v1{
+public class md5Hybrid{
 	
 	void md5Hybrid(){
 	//code here
 	}
 	
 	public static void main(String [] args){
+	
+	
+	
+	int[] mcode = new int[] {3,5,6,4,1,2,0,7};	
+	
+	
 		
-		//inputs
-		//plainTextPassString
-		//totalDigitCodes
+	String str = md5hybrid("rolly", mcode);
 		
-		String plainTextPassString = "rolly";
-		//DISPLAY MCODE		
-		System.out.println("PLAIN TEXT PASSWORD = " + plainTextPassString);
+	System.out.println(str);	
+	for(int i=0; i<8; i++)System.out.print(mcode[i]);		
 		
 		
-		
-		
-		int[] mcode = new int[] {5,0,4,1,6,2,3,7,8,9};
-		
-			
-		//DISPLAY MCODE		
-		System.out.print("MCODE = ");
-		for (int i = 0; i < mcode.length; i ++){
-			
-			System.out.print(" " + mcode[i]);
-		}
+	}//main 
 
+
+
+
+	public static String md5hybrid(String pass, int[] mcode){
+		
 		//cound how many digit the mcode 
 		int counter = 0;
-		for (int i = 0; i < mcode.length; i ++)
-			//if (mcode[i] != null)
-				counter ++;		
-		int totalDigitCodes = counter;
-		System.out.println("");
-		System.out.println("HOW MANY DIGIT IN MCODE = " + counter);		
+		for (int i = 0; i < mcode.length; i ++) counter ++;	
 		
-		//declare
-		String hashedTextPassString;
-		//hash it
-		hashedTextPassString = MD5(plainTextPassString);
-		//display md5 hash
+		//hash the passw2ord
+		String hashedTextPassString = MD5(pass);
 		
-		System.out.println("MD5 STRING = " + hashedTextPassString);
-	
-		//convert md5 hash strin to char array
+		//convert md5 hash string into char array
 		char[] hashedTextPassCharArr; 
-		hashedTextPassCharArr = hashedTextPassString.toCharArray();
-		
-		//DISPLAY MD5 CHAR ARRAY		
-		System.out.print("MD5 CHAR ARRAY = ");
-		for (int i = 0; i < hashedTextPassCharArr.length; i ++){
-			
-			System.out.print("" + hashedTextPassCharArr[i]);
-		}
-		
-		//
-		//cound how many character is the md5hash 
-		int counter_a = 0;
-		for (int i = 0; i < hashedTextPassCharArr.length; i ++)
-			//if (mcode[i] != null)
-				counter_a ++;
-			
-		int hashedTextPassCharArrLength = counter_a; //hashedTextPassCharArr.length;		
-		System.out.println("");
-		System.out.println("TOTAL CHARACTERS IN MD5 CHAR ARRAY HASH = " + counter_a);		
-		
-		
+		hashedTextPassCharArr = hashedTextPassString.toCharArray();	
 
-		
 		//CALCULATE CHUNCK LENGTH
-		int chunkLenght = hashedTextPassCharArrLength/(totalDigitCodes-1);							
+		int chunkLenght = 32/(counter-1);							
 		System.out.println("chunkLenght = " + chunkLenght);	
-		
-		String strArr   = "";
-		String strArr1  = "";
-		int n=0,l=0,m=0;
-		
-			
-			//INSERT COMMA BETWEEN CHUNKS	
-			for(l=1;l < hashedTextPassCharArrLength;l++){
-				++n;
-				strArr += hashedTextPassCharArr[l-1];
-				if(n == chunkLenght) { strArr += ","; n = 0;}
-			}
-		
 
-		//separate delimited string
-		String[] output = strArr.split(",");
+		//INSERT COMMA BETWEEN CHUNKS
+		int l, n = 0;
+		String strArr = "";
+		for(l=0;l < 32;l++){
+			++n;
+			strArr += hashedTextPassCharArr[l];
+			if(n == chunkLenght) { strArr += ","; n = 0;}
+		}//
 		
+		//separate delimited string
+		String[] output = strArr.split(",");		
+
 
 		//count how many index in the array		
-		int counter1		= 0;
+		int index_count = 0;
 		for (int i = 0; i < output.length; i++)
-			if (output[i] != null) counter1++;
-			
-		
-		System.out.print("ORIGINAL HASH ");		
-		
-		//display array	
-		for(int i=0; i < counter1;i++){
-			System.out.print(output[i] + " ");	
-		}			
-		
-		//int howManyDigit;
-		//howManyDigit = counter1;
-	
-		
+			if (output[i] != null) index_count++;
+
+
 		//SCRAMBLER!
 		int j,i;
 		String scrambledEgg = "";
-		for(j = 0; j != totalDigitCodes; j++){		
-			for(i = 0; i != (totalDigitCodes+1); i++){
+		for(j = 0; j != 8; j++){		
+			for(i = 0; i != (8+1); i++){
 				if(mcode[j] == i){
 					scrambledEgg += output[i] + " ";					
 				}	
 			}		
-		}//for j;	
+		}//for j;			
 		
 		
-		//display mcode
-		//for(i=0; i < counter;i++){
-		//	System.out.print(mcode[i] + " ");
-		//}	
 		
 		
-		System.out.println("");
-		System.out.println("SCRAMBLED HASH = " + scrambledEgg);
+		/******************************/
+		return scrambledEgg;
 		
-		
-	}//main 		
+	}
 	
 	public static String MD5(String md5) {
 		try {
